b766a4eeb2e97a2928116259cc556f5b28f3fcae
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-02-02 17:07:56 -0800

    Added state to make player name editable

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((prevState) => {
+      return !prevState;
+    });
+  };
   return (
     <li>
       <span className='player'>
-        <span className='player-name'>{name}</span>
+        <span className='player-name'>
+          {isEditing ? <input type='text' required value={name} /> : name}
+        </span>
         <span className='player-symbol'>{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 };
